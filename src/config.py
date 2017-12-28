c10588df3bf2ad91dff5f565b10413dd97560bee
QuidWallet <quidwallet@gmail.com>
2017-12-28 18:41:23 +0300

    top50 account choose by lowest price

--- src/config.py
+++ src/config.py
@@ -4,4 +4,13 @@ CRYPTOCOMPARE_TOKENS_URL = "https://min-api.cryptocompare.com/data/all/\
 coinlist"
 TOKENS_LIST_FILENAME = "tokens/tokens.json"
 TOKENS_EXCEPTIONS_FILENAME = "tokens/exceptions.json"
+
 TOKEN_ICON_HEIGHT = 30  # 30 px for 1x
+
+# TOP TOKENS
+TOP_TOKENS_LIMIT = 50
+TOP_TOKENS_FILENAME = "tokens/top_tokens.json"
+
+#EXCHANGES = "Cryptsy,BTCChina,Bitstamp,BTER,OKCoin,Coinbase,Poloniex,Cexio,BTCE,BitTrex,Kraken,Bitfinex,Yacuna,LocalBitcoins,Yunbi,itBit,HitBTC,btcXchange,BTC38,Coinfloor,Huobi,LakeBTC,ANXBTC,Bit2C,Coinsetter,CCEX,Coinse,MonetaGo,Gatecoin,Gemini,CCEDK,Cryptopia,Exmo,Yobit,Korbit,BitBay,BTCMarkets,Coincheck,QuadrigaCX,BitSquare,Vaultoro,MercadoBitcoin,Bitso,Unocoin,BTCXIndia,Paymium,TheRockTrading,bitFlyer,Quoine,Luno,bitFlyerFX,TuxExchange,CryptoX,Liqui,MtGox,BitMarket,LiveCoin,Coinone,Tidex,Bleutrade,EthexIndia,Bithumb,CHBTC,ViaBTC,Jubi,Zaif,Novaexchange,WavesDEX,Binance,Lykke,Remitano,Coinroom,Abucoins,BXinth,Gateio,HuobiPro,OKEX"
+
+EXCHANGES = "Binance,Poloniex,HitBTC,Liqui"
