60abeeec7ac232ec767fa6d955014f1cf3f09f24
QuidWallet <quidwallet@gmail.com>
2018-01-19 19:03:01 +0300

    available currencies composer added

--- compose_available_currencies.py
+++ compose_available_currencies.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from src.utils import (write_json_to_file,
-                       load_json_from_file,
+                       load_json_from_url,
                        load_json_from_url,                       
                        print_stats)
 
@@ -10,7 +10,7 @@ CHECK_URL = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms='
 
 def run():
     lst = []
-    currencyDct = load_json_from_file('data/currencies_list.json')
+    currencyDct = load_json_from_url('https://raw.githubusercontent.com/smirzaei/currency-formatter/master/currencies.json')
     print "Currencies loaded: " + str(len(currencyDct))
     i = 0
     for code in currencyDct.keys():
@@ -23,11 +23,12 @@ def run():
         else:
             currency = currencyDct[code]
             # print currency
-            lst += [currency]
+            lst += [code]
         
         print "----"
     print "Currencies OK: " + str(len(lst))
-    write_json_to_file(lst, 'result/currency_list/currency_list.json')
+    
+    write_json_to_file(sorted(lst), 'result/currency_list/currency_list.json')
     print "Currencies written to file "
 
 
